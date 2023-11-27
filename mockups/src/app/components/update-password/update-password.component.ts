67c98b8674d2a6be07d790dc238da1a294df33aa
SantiagoBetancourth <110997727+SantiagoBetancourth@users.noreply.github.com>
2023-11-27 11:36:34 -0500

    Actualizar contraseña

--- mockups/src/app/components/update-password/update-password.component.ts
+++ mockups/src/app/components/update-password/update-password.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+// update-password.component.ts
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-update-password',
   templateUrl: './update-password.component.html',
-  styleUrls: ['./update-password.component.css']
+  styleUrls: ['./update-password.component.css'],
 })
-export class UpdatePasswordComponent {
+export class UpdatePasswordComponent implements OnInit {
+  passwordForm: FormGroup = this.fb.group({
+    currentPassword: ['', Validators.required],
+    newPassword: ['', [Validators.required, Validators.minLength(6)]],
+    confirmPassword: ['', Validators.required],
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {}
+
+  onSubmit() {
+    if (this.passwordForm.valid) {
+      // Aquí puedes agregar la lógica para enviar la solicitud de actualización de contraseña
+      // Puedes acceder a los valores del formulario utilizando this.passwordForm.value
+      console.log(this.passwordForm.value);
+      // También puedes hacer la llamada HTTP para actualizar la contraseña en el servidor
+    }
+  }
 }
